f94aaf2dbaa86a44b7db8574619e2bd694e040bc
NAndAxe <98951594+NAndAxe@users.noreply.github.com>
2025-03-14 13:42:24 +0100

    project done

---
+++ data/data.js
@@ -0,0 +1,7 @@
+const tomb = [
+  { cim: "Film1", rendez: "Rendez1", megjelen: "Megjelen1", oszkar: "Igen" },
+  { cim: "Film2", rendez: "Rendez2", megjelen: "Megjelen2", oszkar: "Igen" },
+  { cim: "Film3", rendez: "Rendez3", megjelen: "Megjelen3", oszkar: "Nem" },
+];
+
+export default tomb;
